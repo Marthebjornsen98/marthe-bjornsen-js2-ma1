0496844e3b59d14fb3ccd46e4d435a65c39bc999
Marthe Bjornsen <69841912+Marthebjornsen98@users.noreply.github.com>
2021-09-26 22:21:38 +0200

    confused

--- js/script.js
+++ js/script.js
@@ -1,37 +1,23 @@
 import fetchData from "./libs/fetch-data.js";
 import { filteringArray } from "./libs/filter.js";
 
-const data = await fetchData("https://fakestoreapi.com/products");
+let data = await fetchData("https://fakestoreapi.com/products");
 console.log("data", data);
 
-const cardContainer = document.querySelector(".container__card");
+const search = document.querySelector(".search");
+const searchResult = document.querySelector(".search__result");
 
-data.forEach((elm) => {
-  cardContainer.innerHTML += `
-    <div class="col-sm-3">
-        <div class="card">
-            <img src="${elm.image}" alt="Products" class="card__img">
-            <div class="card-body">
-                <h2 class="card-title">${elm.title}</h2>
-                <h3 class="card__price">${elm.price} $</h3>
-                <p class="card-text">${elm.description}</p>
-                <a href="#" class="btn btn-primary card__btn">Add to bag</a>
-            </div>
-        </div>
-    </div>
-  `;
-});
+search.onkeyup = function () {
+  searchResult.innerHTML = "";
+  let filteredBy = filteringArray(data, search.value);
 
-const searchBar = document.querySelector(".search");
+  if (filteredBy.length === 0) {
+    searchResult.innerHTML = "No results, the price is too low.";
+    return;
+  }
 
-searchBar.onkeyup = function () {
-  cardContainer.innerHTML = "";
-
-  const sortBy = filteringArray(data, searchBar.value);
-  console.log(sortBy);
-
-  sortBy.forEach((elm) => {
-    cardContainer.innerHTML += `
+  filteredBy.forEach((elm) => {
+    searchResult.innerHTML += `
         <div class="col-sm-3">
             <img src="${elm.image}" class="card-img-top" alt="">
             <div class="card-body">

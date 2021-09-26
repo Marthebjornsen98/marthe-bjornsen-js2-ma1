301cca14e8977c1741938b2d429b3712934da845
Marthe Bjornsen <69841912+Marthebjornsen98@users.noreply.github.com>
2021-09-26 20:32:35 +0200

    fixing stuff

--- js/libs/fetch-data.js
+++ js/libs/fetch-data.js
@@ -1,13 +1,10 @@
-// import message from "../components/message.js";
-
 export default async function fetchData(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-
     return data;
   } catch (error) {
-    message("danger", `The error is: ${error} `, ".alert");
+    console.log(error);
   } finally {
   }
 }
--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ const data = await fetchData("https://fakestoreapi.com/products");
 console.log("data", data);
 
 const cardContainer = document.querySelector(".container__card");
-console.log(cardContainer);
 
 data.forEach((elm) => {
   cardContainer.innerHTML += `
@@ -14,30 +13,34 @@ data.forEach((elm) => {
             <img src="${elm.image}" alt="Products" class="card__img">
             <div class="card-body">
                 <h2 class="card-title">${elm.title}</h2>
+                <h3 class="card__price">${elm.price} $</h3>
                 <p class="card-text">${elm.description}</p>
-                <a href="#" class="btn btn-primary card__btn">Add to favourites</a>
+                <a href="#" class="btn btn-primary card__btn">Add to bag</a>
             </div>
         </div>
     </div>
   `;
 });
 
-let searchBar = document.querySelector(".search");
-console.log(searchBar);
+const searchBar = document.querySelector(".search");
+
 searchBar.onkeyup = function () {
   cardContainer.innerHTML = "";
 
-  let sortedByPrice = filteringArray(data, searchBar.value);
-  console.log(sortedByPrice);
+  const sortBy = filteringArray(data, searchBar.value);
+  console.log(sortBy);
 
-  for (let i = 0; i < sortedByPrice.length; i++) {
-    cardContainer.innerHTML += `<div class="col-sm-3">
-      <img src="${sortedByPrice[i].image}" class="card-img-top" alt="">
-      <div class="card-body">
-        <h5 class="card-title">${sortedByPrice[i].title}</h5>
-        <p class="card-text">${sortedByPrice[i].description}</p>
-        <a href="#" class="btn btn-primary">Add to favourites</a>
-      </div>
-    </div>`;
-  }
+  sortBy.forEach((elm) => {
+    cardContainer.innerHTML += `
+        <div class="col-sm-3">
+            <img src="${elm.image}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h2 class="card-title">${elm.title}</h2>
+                <h3 class="card__price">${elm.price}</h3>
+                <p class="card-text">${elm.description}</p>
+                <a href="#" class="btn btn-primary card__btn">Add to bag</a>
+            </div>
+        </div>
+    `;
+  });
 };

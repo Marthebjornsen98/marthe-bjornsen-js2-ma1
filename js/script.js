6284b2c404f69ad1a50f56e5a5a77691ad797a63
Marthe Bjornsen <69841912+Marthebjornsen98@users.noreply.github.com>
2021-09-26 20:12:09 +0200

    doing stuff

--- js/script.js
+++ js/script.js
@@ -11,28 +11,16 @@ data.forEach((elm) => {
   cardContainer.innerHTML += `
     <div class="col-sm-3">
         <div class="card">
-            <img src="${elm.image}" alt="Image" class="card__img">
+            <img src="${elm.image}" alt="Products" class="card__img">
             <div class="card-body">
                 <h2 class="card-title">${elm.title}</h2>
                 <p class="card-text">${elm.description}</p>
-                <a href="#" class="btn btn-primary">Add to favourites</a>
+                <a href="#" class="btn btn-primary card__btn">Add to favourites</a>
             </div>
         </div>
     </div>
   `;
 });
-
-// for (let i = 0; i < data.length; i++) {
-//   container.innerHTML += `
-//     <div class="col-sm-3">
-//         <img src="${data[i].image}" class="card-img-top" alt="">
-//         <div class="card-body">
-//             <h5 class="card-title">${data[i].title}</h5>
-//             <p class="card-text">${data[i].description}</p>
-//       <a href="#" class="btn btn-primary">Add to favourites</a>
-//     </div>
-//   </div>`;
-// }
 
 let searchBar = document.querySelector(".search");
 console.log(searchBar);
